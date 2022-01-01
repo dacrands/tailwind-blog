770473fe337f2317b3450afcaea72bcaf8010b23
da crands <davecrands@gmail.com>
2022-01-01 10:07:07 -0500

    Wrap root with theme provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,14 @@
+import React, { useState } from "react"
 // custom CSS styles
 import "./src/styles/global.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
+import { ThemeProvider } from "./src/context/context"
+
+
+export const wrapRootElement = ({ element }) => {  
+  return <ThemeProvider>
+    {element}
+  </ThemeProvider>
+}
